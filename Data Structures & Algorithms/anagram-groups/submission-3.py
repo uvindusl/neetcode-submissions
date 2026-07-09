a24883a3df74b064fbc8caaf093b32986237b1d4
Uvindu Sankalpa <125727792+uvindusl@users.noreply.github.com>
2026-07-09 23:14:50 +0530

    Add: anagram-groups - submission-3

---
+++ Data Structures & Algorithms/anagram-groups/submission-3.py
@@ -0,0 +1,26 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        values = []
+        tmp_values = []
+        check_values = list(strs)
+        for i in range(len(strs)):
+            value1 = strs[i]
+            if value1 in check_values:
+                tmp_values = []
+                tmp_values.append(value1)
+                check_values.remove(value1)
+                for j in range(i+1, len(strs)):
+                    value2 = strs[j]
+                    if value1 == value2:
+                        tmp_values.append(value2)
+                        check_values.remove(value1)
+                    else:
+                        if len(value1) == len(value2):
+                            if sorted(value1) == sorted(value2):
+                                tmp_values.append(value2)
+                                check_values.remove(value2)
+
+                values.append(tmp_values)
+                # tmp_values = []
+
+        return values
